e45e73c4c5e58a9e420190e6d84d8a8ae546e5ea
yaadaulakh <yaadkamal078aulakh@outlook.com>
2024-12-03 09:41:45 -0500

    changed background color
    changed the background color from blue to grey

--- themes/jatt-child/functions.php
+++ themes/jatt-child/functions.php
@@ -12,3 +12,10 @@
     wp_enqueue_style( 'child-style', get_stylesheet_uri());
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts');
+
+function enqueue_logged_in_styles() {
+    if (is_user_logged_in()) {
+        wp_enqueue_style('logged-in-css', get_stylesheet_directory_uri() . '/jatt.css', array(), '1.0.0');
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_logged_in_styles');
